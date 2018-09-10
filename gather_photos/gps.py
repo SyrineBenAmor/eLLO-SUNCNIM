3fd47a5e8573df7bf7cc9407527b85150e777b67
Syrine <syrine.benamor76@gmail.com>
2018-09-10 15:08:06 +0200

    add gps.py to the file gather_photos

---
+++ gather_photos/gps.py
@@ -0,0 +1,17 @@
+'''Prints the latitude and longitude every second.'''
+import time
+import microstacknode.hardware.gps.l80gps 
+
+gps = microstacknode.hardware.gps.l80gps.L80GPS()
+
+def getGPSvalue():
+    data = gps.get_gprmc()
+    lat = data.get("latitude")
+    long = data.get("longitude")
+    print(lat,long)
+    file = open("GPSvalue.txt","a+")
+    file.write("{},{},{}\n".format(time.time(),lat,long))
+    time.sleep(1)
+        
+    file.close()
+    return (lat,long)
