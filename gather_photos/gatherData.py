beac78fac01a88611d8a6c4d9aac27647fdbe962
Syrine <syrine.benamor76@gmail.com>
2018-09-17 15:21:08 +0200

    write all data from GPS and acceleromter in one file

--- gather_photos/gatherData.py
+++ gather_photos/gatherData.py
@@ -4,25 +4,24 @@ from fractions import Fraction
 from datetime import datetime, timedelta
 import os
 
-
-startHour   = 15
-startMinute = 25
-finishHour  = 15
-finishMinute = 59
-
-
 import Accel
 import gps
 
+startHour   = 14
+startMinute = 49
+finishHour  = 14
+finishMinute = 59
+
+
 with picamera.PiCamera() as camera :
-    #************fix the values to take photos with same terms of brightness
+#************fix the values to take photos with same terms of brightness
     #choose resolutionof camera (camera type 2)
     camera.resolution = (2560, 1920)
     #low light : very slow framerate (1/6) (Max framerate = 15 )
     f= camera.framerate
     f= Fraction(1 ,8)
     #wait for the automatic gain control to settle
-    #time.sleep(2)
+    time.sleep(2)
     #choose the exposure time
     camera.shutter_speed = camera.exposure_speed
     camera.exposure_mode = 'off'
@@ -32,10 +31,10 @@ with picamera.PiCamera() as camera :
     g = camera.awb_gains
     camera.awb_mode = 'off'
     camera.awb_gains = g
-    #wait 2sec
-    #time.sleep(2)
-        
-    #***********take photos*****************************
+    #wait 1sec
+    time.sleep(0.5)
+#***********take photos*****************************
+
 
     print("lets start")
     #wait until time = start time
@@ -44,15 +43,17 @@ with picamera.PiCamera() as camera :
         time.sleep(30)
     os.system("mkdir /home/pi/Desktop/eLLO-SUNCNIM/gather_photos/test."+time.strftime("%d-%m-%Y"))
     print("Folder Created")
+    file = open(time.strftime("%d-%m-%Y")+"Data.txt","a+")
+    file.write("Time(s), AngleX(°); AngleY(°);AngleZ(°), DistanceX[mm];DistanceY[mm];DistanceZ[mm], AccelX[mm/s^2];AccelY[mm/s^2];AccelZ[mm/s^2], Latitude Longitude\n")
+    file.close()
+
     #while time is different of finish time take photos
     while not((datetime.now().time().hour == finishHour) and (datetime.now().time().minute == finishMinute)):
-        print ("time pour prendre une photo",datetime.now().time())
         camera.capture_sequence(["/home/pi/Desktop/eLLO-SUNCNIM/gather_photos/test."+time.strftime("%d-%m-%Y")+"/"+"img"+time.strftime("%H:%M:%S")+".jpg"])
-        
+        print ("picture")
         _, _, _ ,ms= Accel.auto_calibration()
         DxF,DyF,DzF = Accel.gatherDistance(ms)
         Lat,long= gps.getGPSvalue()
-        #time.sleep(2) #wait 2 sec
-        print ("picture")
+        
+        
     print('End')
-
