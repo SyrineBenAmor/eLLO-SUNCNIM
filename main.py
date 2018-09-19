8b8c9e7fbe848ef95d039243c7bb521c26c319c2
Syrine <syrine.benamor76@gmail.com>
2018-09-19 12:50:28 +0200

    change real coordinate value

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ planSite ="GPS/Prototype vue de Haut.jpg"
 #*************************************************************************
 def main():
     
-    realCoordinate = (4.46,18.7)
+    realCoordinate = (1.66,1.5)
     siteMap = pinMap(planSite)
     
     image = siteMap.brokenMirrors(realCoordinate)
