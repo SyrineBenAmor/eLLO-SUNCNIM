bff56d0f8d5c3ee15d6bcf921cc99b4b461b159c
Syrine BEN AMOR <syrine.benamor76@gmail.com>
2018-10-14 19:02:37 +0200

    add path of image call location function

--- main.py
+++ main.py
@@ -8,18 +8,23 @@ import Accel
 
 sys.path.append('gather_Data/GPS/')
 sys.path.append('find_crack_in_map/')
+import ComparaisonTimeImgwithData as location
 import plan
 from plan import pinMap
 
 sys.path.append('Image_processing/')
 import Crack_Detection as detection
 
+
 then = time.time()
 planSite ="find_crack_in_map/Prototype vue de Haut.jpg"
 
+pathImage = "Image_processing/image_processing_output/"+time.strftime("%d-%m-%Y")+"fissure/"+"*.jpg"
+nameDatafile = "gather_Data/data/"+time.strftime("%d-%m-%Y")+".txt"
 #*************************************************************************
 def main():
     detection.detectcrack()
+    location.comparaisonBetweenImageTimeAndDataTime(nameDatafile,pathImage)
     """
     realCoordinate = (7,1)
     siteMap = pinMap(planSite)
