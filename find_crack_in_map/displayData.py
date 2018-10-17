c2c53e3273073de557d45e930e3571b5a0c57246
Syrine BEN AMOR <syrine.benamor76@gmail.com>
2018-10-17 17:01:25 +0200

    delete all programm and think with for creating class

--- find_crack_in_map/displayData.py
+++ find_crack_in_map/displayData.py
@@ -9,44 +9,4 @@ import cv2
 import time
 
 
-
-def transformTimeToSec (time): #(%H:%M:%S)
-    h,m,s = time.split(":")
-    time_in_sec = int(h) * 3600 + int(m) * 60 +int(s)
-    return (time_in_sec)
-
-def timeDataTransform(TimeInDataFile):
-    divide_Time = time.localtime(TimeInDataFile)
-    hour  = time.localtime(TimeInDataFile)[3]
-    minute = time.localtime(TimeInDataFile)[4]
-    sec = time.localtime(TimeInDataFile)[5]
-    return hour,minute,sec
-
-def displayData(nameDatafile,pathImage):
-    for imagePath in glob.glob(pathImage):
-        i+=1
-        image_time = imagePath.split('.')[-2]
-        image_time_hour = image_time.split(':')[0]
-        image_time_minute = image_time.split(':')[1]
-        image_time_sec = image_time.split(':')[2]
-    
-        fichier = open(nameDatafile,'r') 
-
-        for ligne in fichier:
-            print(ligne)
-            time_data=ligne.split(",")[0]
-            hour,minute,sec =timeDataTransform(int(time_data))
-            if hour==image_time_hour and minute==image_time_minute and sec==image_time_sec:
-                print("Hello" ,ligne)
-            else: 
-                print("n existe pas")
-        fichier.close()
-
-    k = cv2.waitKey(0)
-    if k == 27: #wait for ESC key to exit
-        cv2.destroyAllWindows()
-    print ('end')
-    
-if __name__== "__main__":
-    main()
     
