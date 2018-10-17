e0bfc08f1b0957bbc161fbc8c3cdf8ec65430cbc
Syrine BEN AMOR <syrine.benamor76@gmail.com>
2018-10-17 17:01:51 +0200

    delete unused comment

--- find_crack_in_map/displayData.py
+++ find_crack_in_map/displayData.py
@@ -1,11 +1,4 @@
-# save treated image with same name of the original imag
-# open picture file
-# read pictures name
-#stock picture name
-#comparaison name picture with la colonne du temps dans le fichier data.*txt
-# afficher la ligne qui correspond au meme temps
 import glob
-import cv2
 import time
 
 
