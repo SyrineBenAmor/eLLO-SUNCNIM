18fac6c3831841b5b939d98bd037735dc728f648
Syrine BEN AMOR <syrine.benamor76@gmail.com>
2018-10-17 17:03:37 +0200

    create fetchData() class that need path of photos and path of dataFile.txt and return all coordinate gps of cracked mirror

--- find_crack_in_map/displayData.py
+++ find_crack_in_map/displayData.py
@@ -1,5 +1,11 @@
 import glob
 import time
 
+class fetchData():
+    def __init__(self,pathImage,pathDatafile):
+        self.pathImage = pathImage
+        self.pathDatafile = pathDatafile
+        self.timeImageArray = self.timeImage()
+        self.gpsDataArray = self.timeData()
 
     
