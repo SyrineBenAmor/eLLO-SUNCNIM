f8a4958617878e5e7e567d1867f4d5d652cea2ab
Syrine BEN AMOR <syrine.benamor76@gmail.com>
2018-10-17 17:14:22 +0200

    link between classes

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ sys.path.append('gather_Data/GPS/')
 import gps
 
 sys.path.append('find_crack_in_map/')
-import displayData as location
+import displayData
+from displayData import fetchData 
 import plan
 from plan import pinMap
 
@@ -28,26 +29,29 @@ planSite ="find_crack_in_map/Prototype vue de Haut.jpg"
 pathImage = "Image_processing/image_processing_output/"+time.strftime("%d-%m-%Y")+"/fissure/*.jpg"
 pathDatafile = "gather_Data/data/"+time.strftime("%d-%m-%Y")+".txt"
 
-startHour   = 10
-startMinute = 21
-finishHour  = 10
-finishMinute = 59
+startHour   = 13
+startMinute = 13
+finishHour  = 13
+finishMinute = 14
 
 #*************************************************************************
 def main():
-    #gatherData.gatherData(startHour,startMinute,finishHour,finishMinute)#gather data until Time cleaning finish
-    while not((datetime.now().time().hour > finishHour) and (datetime.now().time().minute > finishMinute)): #if time cleaning finish
-        detection.detectcrack()# treat image and stock them in other folder(caisson folder, fissure folder,ligne de separation folder)
-        location.displayData(nameDatafile,pathImage)#display acceleration, longitude and lattitude
-    """
-    realCoordinate = (7,1)
-    siteMap = pinMap(planSite)
     
-    image = siteMap.brokenMirrors(realCoordinate)
+    #gatherData.gatherData(startHour,startMinute,finishHour,finishMinute)#gather data until Time cleaning finish
+    #while not((datetime.now().time().hour > finishHour) and (datetime.now().time().minute > finishMinute)): #if time cleaning finish
+        #detection.detectcrack()# treat image and stock them in other folder(caisson folder, fissure folder,ligne de separation folder)
+    gpsDataOfcrackedMiror = fetchData(pathImage,pathDatafile)#display acceleration, longitude and lattitude
+    gpsDataArray = gpsDataOfcrackedMiror.gpsDataCouple()
+    print(gpsDataArray)
+    
+    siteMap = pinMap(planSite)
+    for i in range (0,len(gpsDataArray)):
+        print(gpsDataArray[i])
+        image = siteMap.brokenMirrors(gpsDataArray[i])
     cv2.imshow("Image with red caisson ", image)
     
     print("Execution Time = "+str(float(time.time() - then)) + " s")
-    """
+   
     #********************close condition*************************************
 
     k = cv2.waitKey(0)
